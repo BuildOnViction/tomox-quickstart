bbe77f63af8d8ee2fe2e3d997abc89e18b28b06c
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-05-10 04:15:39 +0000

    add testnet support

--- get-tomox-testnet.sh
+++ get-tomox-testnet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-APP_VERSION=1.2.0
+APP_VERSION=1.1.0-beta
 
 cd /tmp && \
     wget -O "tomox-quickstart-${APP_VERSION}.tar.gz" https://github.com/tomochain/tomox-quickstart/archive/v${APP_VERSION}.tar.gz && \
     tar xzf "tomox-quickstart-${APP_VERSION}.tar.gz" && \
     rm -rf "tomox-quickstart-${APP_VERSION}.tar.gz" && \
-    cd "tomox-quickstart-${APP_VERSION}" && bash setup.sh mainnet
+    cd "tomox-quickstart-${APP_VERSION}" && bash setup.sh testnet
