639ccc7a91f52fc4be4e4aceb308b0dd6ab7f9c3
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-05-25 10:30:29 +0000

    tomochain v2.2.2

--- get-tomox.sh
+++ get-tomox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APP_VERSION=1.2.2
+APP_VERSION=1.2.3
 
 cd /tmp && \
     wget -O "tomox-quickstart-${APP_VERSION}.tar.gz" https://github.com/tomochain/tomox-quickstart/archive/v${APP_VERSION}.tar.gz && \
